cc7d9356f76e91b4b53fa0f7a4eacd8641c25da7
mbendev <mathieu.ben@univ-rennes2.fr>
2019-11-13 16:47:35 +0100

    Add super() in __repr__() method of class ProduitPerissable

--- produit.py
+++ produit.py
@@ -64,13 +64,7 @@ class ProduitPerissable(Produit):
         return datetime.now() > self.dateFab + self.dureeConso
     
     def __repr__(self):
-        chaine = "Référence (EAN) : {}\n".format(self.numEAN)
-        chaine += "Identifiant : {}\n".format(self.id)
-        chaine += "Nom : {}\n".format(self.nom)
-        chaine += "Date de fabrication : {}\n".format(self.dateFab.strftime("%d/%m/%Y"))
-        chaine += "En promo : {}\n".format("oui" if self.enPromo() else "non")
-        chaine += "Prix de base : {:.2f}\n".format(self.prixBase)
-        chaine += "Prix actuel : {:.2f}\n".format(self.calculerPrixActuel())
+        chaine = super().__repr__()
         dateLimite = self.dateFab + self.dureeConso
         chaine += "A consommer avant le : {}\n".format(dateLimite.strftime("%d/%m/%Y"))
         if self.dernierJour():
